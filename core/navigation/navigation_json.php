ea12b343fe3e23122476ca214680b4516fef9b24
Janis <hau@haulyshit.de>
2016-04-30 15:22:10 +0200

    Delete batch file

--- core/navigation/navigation_json.php
+++ core/navigation/navigation_json.php
@@ -2,37 +2,34 @@
 
 // echo '<a href="#">hallo welt</a>';
 
-$jsonfile = file_get_contents('core/navigation/menu.json');
+$jsonfile = file_get_contents( 'core/navigation/menu.json' );
 
-$jsonarray = json_decode($jsonfile);
+$jsonarray = json_decode( $jsonfile );
 
 // printr($jsonarray);
 ?>
 <ul>
 
 	<?php
-	foreach ($jsonarray->menu as $dirs) {
+	foreach ( $jsonarray->menu as $dirs ) {
 
 		// printr($mydata);
-		foreach ($dirs as $dir) {
+		foreach ( $dirs as $dir ) {
 
-			foreach ($dir as $value) {
+			foreach ( $dir as $value ) {
 				// printr($value);
-				
+
 				echo "<li><a href='$value->dirurl'>$value->dirname</a><ul>";
-				
-				foreach ($value->links as $value) {
+
+				foreach ( $value->links as $value ) {
 					echo "<li><a href='$value->url'>$value->value</a></li>";
 				}
-				
+
 				echo "</ul></li>";
 
 			}
 
 		}
-		// foreach ($mydata->values as $values) {
-		// echo $values -> value . "\n";
-		// }
 
 	}
 ?>
