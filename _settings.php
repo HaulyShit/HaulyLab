d5ab7d1130163039afe7b458e31bd739bd50ae9f
Janis <hau@haulyshit.de>
2016-04-30 20:42:04 +0200

    Add aditional informations

--- _settings.php
+++ _settings.php
@@ -2,7 +2,7 @@
 /*
  * MENU
  * If you don't want use the default navigation and you want use your own
- * specific links then set the $menuJson as true and edit the
+ * specific links, then set the $menuJson as true and edit the
  * core/navigation/menu.json.
  */
 // $menuJson = TRUE;
@@ -12,7 +12,11 @@ $menuJson = FALSE;
  * BREAKPOINTS
  * This give you the possibility to change the viewport-resize-action-settings
  * Haulylab works with the indices "end" and "start". But you can delete this
- * indices in the sub array of $haulylabBreakpoints
+ * indices in the sub array of $haulylabBreakpoints.
+ *
+ * But keep in mind if you don't use "end" then Haulylab thinks: "okay 'end' is the
+ * window width". If you don't use "start", haulylab use 0 as start.
+ *
  * @formatter:off
  */
 $haulylabBreakpoints = array( 
