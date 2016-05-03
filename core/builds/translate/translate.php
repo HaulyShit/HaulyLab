68f27fc1de5d0ccaddfcbfd54305727517893cd0
Janis Hau <j.hau@neusta.de>
2016-05-03 12:00:20 +0200

    Catch errors, fake classes

--- core/builds/translate/translate.php
+++ core/builds/translate/translate.php
@@ -2,4 +2,33 @@
 function __($ex) {
     echo "$ex";
 }
+
+
+/**
+ * Mockobject
+ */
+class Mockobject {
+	
+	function __call( $name, $arguments ) {
+		return $this;
+	}
+	
+	
+		
+	function __toString(  ) {
+		return "";
+	}
+	
+	
+		
+	
+	
+}
+
+
+
+
+$block= new Mockobject();
+
+
 ?>
--- core/functions.php
+++ core/functions.php
@@ -2,6 +2,32 @@
 // INCLUDE THE SETTINGS
 require_once __DIR__.'/../_settings.php';
 
+// CATCH ERRORS
+$handler = function ( $errno , $errstr) {
+	echo "$errstr";
+	return true;
+};
+set_error_handler($handler, E_ALL );
+function fatal_handler() {
+  $errfile = "unknown file";
+  $errstr  = "shutdown";
+  $errno   = E_CORE_ERROR;
+  $errline = 0;
+
+  $error = error_get_last();
+
+  if( $error !== NULL) {
+    $errno   = $error["type"];
+    $errfile = $error["file"];
+    $errline = $error["line"];
+    $errstr  = $error["message"];
+  }
+}
+register_shutdown_function( "fatal_handler" );
+
+
+
+
 function printr($arr) {
 	echo "<pre>";
 	print_r($arr);
